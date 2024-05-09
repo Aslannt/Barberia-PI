4383341692c47ab640b1c5e6825147246a7400b8
Willi-2512 <167654194+Willi-2512@users.noreply.github.com>
2024-05-09 18:13:47 +0000

    Se actualizo la base de datos

--- barberia pi.sql
+++ barberia pi.sql
@@ -1,15 +1,16 @@
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET NAMES utf8 */;
-/*!50503 SET NAMES utf8mb4 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
+-- Crear la base de datos si no existe
 CREATE DATABASE IF NOT EXISTS `barberia pi` /*!40100 DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci */;
 USE `barberia pi`;
 
+-- Crear la tabla de usuarios
+CREATE TABLE IF NOT EXISTS `usuarios` (
+  `id-usu` int(11) NOT NULL AUTO_INCREMENT,
+  `correo` varchar(50) NOT NULL DEFAULT '',
+  `contraseña` varchar(50) NOT NULL DEFAULT '',
+  PRIMARY KEY (`id-usu`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- Crear la tabla de productos
 CREATE TABLE IF NOT EXISTS `productos` (
   `id-prod` int(11) NOT NULL AUTO_INCREMENT,
   `nombre` varchar(50) NOT NULL DEFAULT '',
@@ -18,7 +19,7 @@ CREATE TABLE IF NOT EXISTS `productos` (
   PRIMARY KEY (`id-prod`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
-
+-- Crear la tabla de reservaciones
 CREATE TABLE IF NOT EXISTS `reservaciones` (
   `id_reser` int(11) NOT NULL AUTO_INCREMENT,
   `nombre` varchar(50) NOT NULL DEFAULT '',
@@ -26,10 +27,12 @@ CREATE TABLE IF NOT EXISTS `reservaciones` (
   `correo` varchar(50) NOT NULL DEFAULT '',
   `teléfono` int(10) NOT NULL,
   `horario` varchar(10) NOT NULL DEFAULT '',
-  PRIMARY KEY (`id_reser`)
+  `id_usu` int(11) NOT NULL, -- Clave externa para relacionar con usuarios
+  PRIMARY KEY (`id_reser`),
+  FOREIGN KEY (`id_usu`) REFERENCES `usuarios`(`id-usu`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
-
+-- Crear la tabla de servicios
 CREATE TABLE IF NOT EXISTS `servicios` (
   `id_serv` int(11) NOT NULL AUTO_INCREMENT,
   `servicio` varchar(50) NOT NULL DEFAULT '',
@@ -37,17 +40,20 @@ CREATE TABLE IF NOT EXISTS `servicios` (
   PRIMARY KEY (`id_serv`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
-
-CREATE TABLE IF NOT EXISTS `usuarios` (
-  `id-usu` int(11) NOT NULL AUTO_INCREMENT,
-  `correo` varchar(50) NOT NULL DEFAULT '',
-  `contraseña` varchar(50) NOT NULL DEFAULT '',
-  PRIMARY KEY (`id-usu`)
+-- Crear la tabla de unión para reservaciones y servicios
+CREATE TABLE IF NOT EXISTS `reservaciones_servicios` (
+  `id_reser` INT(11) NOT NULL,
+  `id_serv` INT(11) NOT NULL,
+  PRIMARY KEY (`id_reser`, `id_serv`),
+  FOREIGN KEY (`id_reser`) REFERENCES `reservaciones`(`id_reser`),
+  FOREIGN KEY (`id_serv`) REFERENCES `servicios`(`id_serv`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
-
-/*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
-/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
+-- Crear la tabla de unión para reservaciones y productos
+CREATE TABLE IF NOT EXISTS `reservaciones_productos` (
+  `id_reser` INT(11) NOT NULL,
+  `id_prod` INT(11) NOT NULL,
+  PRIMARY KEY (`id_reser`, `id_prod`),
+  FOREIGN KEY (`id_reser`) REFERENCES `reservaciones`(`id_reser`),
+  FOREIGN KEY (`id_prod`) REFERENCES `productos`(`id-prod`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
